2b0d21eebd772db2ec5fc510c2c84f9f030892a2
Paula <pogany2003@yahoo.com>
2017-08-18 18:03:57 +0100

    New test added

--- src/test/java/p1/HelloControllerTest.java
+++ src/test/java/p1/HelloControllerTest.java
@@ -1,5 +1,10 @@
 package p1;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.hasEntry;
+
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,8 +19,10 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.context.WebApplicationContext;
 
+import controllers.HelloController;
 import controllers.MyWebConfig;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -40,5 +47,19 @@ public class HelloControllerTest {
                             .attribute("message", "Hello, Spring MVC Framework!");
         MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.get("/hello");
         this.mockMvc.perform(builder).andExpect(ok).andExpect(msg);
+	}
+	
+	@Test
+    public void testHelloControllerViewNameAndMessageAttrVal() throws Exception {
+		HelloController controller = this.wac.getBean(HelloController.class);
+		ModelMap modelMap = new ModelMap();
+		
+		String viewName = controller.printHello(modelMap);
+		String expectedViewName = "helloview";
+		assertEquals(expectedViewName, viewName);
+		
+		String expectedMessageAttrValue = "Hello, Spring MVC Framework!";	
+		assertThat(modelMap, hasEntry("message", (Object) expectedMessageAttrValue));
+		assertThat("Bla Bla", modelMap, hasEntry("message", (Object) expectedMessageAttrValue));
 	}
 }
